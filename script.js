d75aa37ed1a43d40dee8e13b61c0e0040419507d
Anniex09 <cnicolasalexander0807@gmail.com>
2026-04-23 16:40:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+let escribiendo = false;
+let paso = 0;
+
+const imagenes = [
+  "semilla.png",
+  "brote.png",
+  "rosa.png"
+];
+
+const textos = [
+  "Todo comienza con algo pequeño...",
+  "Crece poco a poco, incluso cuando no lo vemos...",
+  "Y un día, florece 🌹"
+];
+
+const boton = document.getElementById("boton");
+const imagen = document.getElementById("imagen");
+
+window.onload = () => {
+  escribirTexto("texto", textos[0]);
+};
+
+function crearLluvia() {
+  const contenedor = document.getElementById("lluvia");
+
+  for (let i = 0; i < 20; i++) {
+    const gota = document.createElement("div");
+    gota.classList.add("gota");
+
+    gota.style.left = Math.random() * 100 + "%";
+    gota.style.animationDuration = (0.5 + Math.random() * 0.5) + "s";
+
+    contenedor.appendChild(gota);
+
+    setTimeout(() => {
+      gota.remove();
+    }, 800);
+  }
+}
+
+function siguientePaso() {
+  if (escribiendo) return; // 
+
+  crearLluvia(); // 
+ 
+  paso++;
+
+  if (paso < imagenes.length) {
+
+    imagen.style.opacity = 0;
+
+    setTimeout(() => {
+      imagen.src = imagenes[paso];
+      imagen.style.opacity = 1;
+      imagen.style.transform = "scale(1.1)";
+    }, 300);
+
+    escribirTexto("texto", textos[paso]);
+
+  } else {
+    mostrarFinal();
+  }
+}
+
+function mostrarFinal() {
+  boton.style.display = "none";
+
+  // Atenuar fondo
+  document.querySelector(".contenedor").style.opacity = 0.3;
+
+  const carta = document.getElementById("carta");
+  carta.classList.remove("oculto");
+
+  setTimeout(() => {
+    carta.classList.add("mostrar");
+  }, 50);
+
+  escribirTexto("mensajeCarta",
+`Hoy no puedo darte una rosa real...
+
+Pero quería darte algo que creciera,
+aunque fuera poco a poco.
+
+Como lo que siento por ti 💌`);
+
+document.getElementById("carta").addEventListener("click", function(e) {
+  if (e.target.id === "carta") {
+    this.classList.remove("mostrar");
+  }
+});
+}
+
+
+function escribirTexto(id, texto) {
+  let i = 0;
+  let velocidad = 40;
+  let elemento = document.getElementById(id);
+  elemento.innerHTML = "";
+
+  escribiendo = true; // 🔒 empieza bloqueo
+
+  function escribir() {
+    if (i < texto.length) {
+      elemento.innerHTML += texto.charAt(i);
+      i++;
+      setTimeout(escribir, velocidad);
+    } else {
+      escribiendo = false; // 🔓 se desbloquea al terminar
+    }
+  }
+
+  escribir();
+}
